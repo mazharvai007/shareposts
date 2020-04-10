fd76d33998c1976fa8e20557c198400322b4626d
swopnil <swopnil420@gmail.com>
2020-04-10 21:06:07 +0600

    Make the posts access control

--- app/controllers/Posts.php
+++ app/controllers/Posts.php
@@ -2,6 +2,13 @@
 
 class Posts extends Controller
 {
+    public function __construct()
+    {
+        if(!isLoggedIn()) {
+            redirect('index.php?url=users/login');
+        }
+    }
+
     public function index()
     {
         $data = [];
